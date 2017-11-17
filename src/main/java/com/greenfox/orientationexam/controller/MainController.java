1933c3882476a516771240346a036a55a99dbf34
Viktor Szabo <viklukacs64@gmail.com>
2017-11-17 09:56:49 +0100

    Exam -- > controller started , model package added

--- src/main/java/com/greenfox/orientationexam/controller/MainController.java
+++ src/main/java/com/greenfox/orientationexam/controller/MainController.java
@@ -1,7 +1,16 @@
 package com.greenfox.orientationexam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @GetMapping(value = "/")
+    public String index(Model model){
+        model.addAttribute("")
+    }
+
+
 }
---
+++ src/main/java/com/greenfox/orientationexam/model/Car.java
@@ -0,0 +1,9 @@
+package com.greenfox.orientationexam.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Car {
+
+
+}
