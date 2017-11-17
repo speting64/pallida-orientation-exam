77f7d6c09f7503407a42edd71c32d78f3613efba
Viktor Szabo <viklukacs64@gmail.com>
2017-11-17 10:23:52 +0100

    Exam -- > Controller - Mainpage initialized

--- src/main/java/com/greenfox/orientationexam/controller/MainController.java
+++ src/main/java/com/greenfox/orientationexam/controller/MainController.java
@@ -1,18 +1,22 @@
 package com.greenfox.orientationexam.controller;
 
+import com.greenfox.orientationexam.repository.CarRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sun.misc.Cache;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    CarRepo carRepo;
+
     @GetMapping(value = "/")
     public String index(Model model){
-        model.addAttribute("cars");
+        model.addAttribute("cars" , carRepo.findAll() );
         return "cars";
 
     }
-
-
 }
