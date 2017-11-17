22d726956b6cd3de685d02041eaf6c19a6f7c908
Viktor Szabo <viklukacs64@gmail.com>
2017-11-17 09:32:05 +0100

    Exam -- > file for the work created

---
+++ src/main/java/com/greenfox/orientationexam/OrientationexamApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.orientationexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientationexamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfox/orientationexam/OrientationexamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.orientationexam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrientationexamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
