2735294202d40f1b959b3ef6047da964dbcdd0a8
Viktor Szabo <viklukacs64@gmail.com>
2017-11-17 10:05:19 +0100

    Exam -- > Model - fields defined

--- src/main/java/com/greenfox/orientationexam/model/Car.java
+++ src/main/java/com/greenfox/orientationexam/model/Car.java
@@ -5,5 +5,49 @@ import javax.persistence.*;
 @Entity
 public class Car {
 
+    String licencePlate;
+    String brand;
+    String model;
+    Integer year;
+    String color;
 
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
