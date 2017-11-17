03cea5c8cafa2045efd9b7873e271d9d518ec976
Viktor Szabo <viklukacs64@gmail.com>
2017-11-17 12:52:05 +0100

    Exam -- > Model -- setting the PoliceCar or Diplomat

--- src/main/java/com/greenfox/orientationexam/controller/MainController.java
+++ src/main/java/com/greenfox/orientationexam/controller/MainController.java
@@ -6,12 +6,15 @@ import com.greenfox.orientationexam.repository.CarRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import sun.misc.Cache;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 public class MainController {
+
+    String plate;
 
     @Autowired
     CarRepo carRepo;
@@ -36,6 +39,30 @@ public class MainController {
             model.addAttribute("cars", carRepo.findAllByBrandIsLike("%" + search + "%"));
         }
         return "cars";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("plate") String plate) {
+        model.addAttribute("plate", carRepo.findOne(plate).getPlate());
+        model.addAttribute("policecar", carRepo.findOne(plate).isPoliceCar());
+        model.addAttribute("diplomat", carRepo.findOne(plate).isDiplomat());
+        this.plate = plate;
+        return "update";
+    }
+
+    @PostMapping("/edit/update")
+    public String update(@RequestParam("/") String plates,
+                         @RequestParam(name = "policecar", required = false) String policeCar,
+                         @RequestParam(name = "diplomat", required = false) String diplomat){
+        LicencePlate save = carRepo.findOne(plate);
+        if(!plates.equals("")){
+            save.setPlate(plates);
+        }
+        save.setPoliceCar(Boolean.valueOf(policeCar));
+        save.setDiplomat(Boolean.valueOf(diplomat));
+        carRepo.save(save);
+        return "redirect:/cars";
+
     }
 
 
